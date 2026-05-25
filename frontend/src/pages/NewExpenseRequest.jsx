108e4c339aa801c0cce2621f96cab81cbab87c15
LeeVuKhang <lvukhangpq@gmail.com>
2026-05-25 17:50:39 +0700

    chore: remove unused svg assets

--- frontend/src/pages/NewExpenseRequest.jsx
+++ frontend/src/pages/NewExpenseRequest.jsx
@@ -495,9 +495,7 @@ export default function NewExpenseRequest({ initialRequest = null, mode = "creat
                   setCategoryId(categoryIdForName(categoryOptions, e.target.value, categoryId));
                 }}
                 style={{
-                  ...baseInput, appearance: "none",
-                  backgroundImage: "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='%23374151' viewBox='0 0 16 16'%3E%3Cpath d='M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/%3E%3C/svg%3E\")",
-                  backgroundRepeat: "no-repeat", backgroundPosition: "right 14px center", paddingRight: 40,
+                  ...baseInput,
                 }}>
                 {categoryOptions.map(c => <option key={c.id ?? c.name} value={c.name}>{c.name}</option>)}
               </select>
